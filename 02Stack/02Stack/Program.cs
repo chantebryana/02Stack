42c1b39824f2bb64ebd7391e1e096eef06476fec
chantebryana <chantebryana@gmail.com>
2019-09-28 10:32:25 -0500

    scaffolding bits

--- 02Stack/02Stack/Program.cs
+++ 02Stack/02Stack/Program.cs
@@ -2,11 +2,35 @@
 
 namespace _02Stack
 {
+    class Stack
+    {
+        private int number;
+
+        public void Push()
+        {
+            Console.WriteLine("Push");
+        }
+
+        public void Pop()
+        {
+            Console.WriteLine("Pop");
+        }
+
+        public void Clear()
+        {
+            Console.WriteLine("Clear");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack();
+
+            stack.Push();
+            stack.Pop();
+            stack.Clear();
         }
     }
 }
