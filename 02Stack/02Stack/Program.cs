325e086be6c53a0570a2d54447e49f6f8d8bd9dd
Chante Earthwell <c.earthwell@gmail.com>
2019-09-30 14:33:04 -0500

    playing with lists

--- 02Stack/02Stack/Program.cs
+++ 02Stack/02Stack/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;   // needed for List
+using System.Linq;   // needed for Last();
 
 namespace _02Stack
 {
     class Stack
     {
-        private int _number;
+        private List<int> _number;
 
         public void Push(int num)
         {
-            _number = num;
+            _number.Add(num);
         }
 
         public int Pop()
         {
-            return _number;
+            var last = _number.Last<int>();
+            _number.Remove(last);
+            return last;
         }
 
         public void Clear()
@@ -31,6 +35,29 @@ namespace _02Stack
             stack.Push(1);
             Console.WriteLine(stack.Pop());
             stack.Clear();
+
+            var numbers = new List<int>();
+            void print(List<int> num)
+            {
+                num.ForEach(i => Console.WriteLine("{0}\t", i));
+                Console.WriteLine("\n");
+            }
+            numbers.Add(1);
+            print(numbers);
+            numbers.Add(2);
+            print(numbers);
+            numbers.Add(3);
+            print(numbers);
+            var lastItem = numbers.Last<int>();
+            Console.WriteLine(lastItem);
+
+            numbers.Remove(3);
+            print(numbers);
+            numbers.Remove(2);
+            print(numbers);
+            numbers.Remove(1);
+            print(numbers);
+
         }
     }
 }
