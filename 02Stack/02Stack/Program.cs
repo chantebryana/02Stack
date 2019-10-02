cb7beea87968b1973db61e2e8aff68dec7e7d90b
chantebryana <chantebryana@gmail.com>
2019-10-02 06:58:14 -0500

    playing with exceptions after receiving Jimbo lecture on up/downcasting

--- 02Stack/02Stack/Program.cs
+++ 02Stack/02Stack/Program.cs
@@ -11,18 +11,38 @@ namespace _02Stack
         public Stack()
         {
             _number = new List<int>();
+            // CE turn this into an <obj> list
         }
 
         public void Push(int num)
         {
-            _number.Add(num);
+            if (num == -1)  // temporarily -1 to make it work with int
+            {
+                Console.WriteLine("You donked: {0}", new InvalidOperationException());
+            }
+            else
+            {
+                _number.Add(num);
+            }
+            //CE create InvalidOperationException if pass null
         }
 
         public int Pop()
         {
-            var last = _number.Last<int>();
-            _number.Remove(last);
-            return last;
+            if (_number == null)
+            {
+                // this doesn't work:
+                Console.WriteLine("You donked: {0}", new InvalidOperationException());
+                return -1;
+            }
+            else
+            {
+                var last = _number.Last<int>();
+                _number.Remove(last);
+                return last;
+            }
+           
+            //CE create InvalidOperationException if Pop is called on empty list
         }
 
         public void Clear()
@@ -37,32 +57,14 @@ namespace _02Stack
         {
             var stack = new Stack();
 
+            Console.WriteLine(stack.Pop());  // this throws its own exception without me...
             stack.Push(1);
+            stack.Push(-1);
+            stack.Push(3);
             Console.WriteLine(stack.Pop());
-            stack.Clear();
-
-            var numbers = new List<int>();
-            void print(List<int> num)
-            {
-                num.ForEach(i => Console.WriteLine("{0}\t", i));
-                Console.WriteLine("\n");
-            }
-            numbers.Add(1);
-            print(numbers);
-            numbers.Add(2);
-            print(numbers);
-            numbers.Add(3);
-            print(numbers);
-            var lastItem = numbers.Last<int>();
-            Console.WriteLine(lastItem);
-
-            numbers.Remove(3);
-            print(numbers);
-            numbers.Remove(2);
-            print(numbers);
-            numbers.Remove(1);
-            print(numbers);
-
+            Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //stack.Clear();
         }
     }
 }
